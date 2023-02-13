0af45c4ae01eeb193ce50213881eb77096dc36b9
comput3202 <comput3202@gmail.com>
2023-02-13 13:44:55 +0900

    2023.02.13.월

--- src/this_Is_Java/chap15_Collection/sec03/HashSetEx02.java
+++ src/this_Is_Java/chap15_Collection/sec03/HashSetEx02.java
@@ -1,10 +1,20 @@
 package chap15_Collection.sec03;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetEx02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//HashSet 拿泛记 积己
+		Set<Member> set = new HashSet<Member>();
+		
+		//Member 按眉 历厘
+		set.add(new Member("全辨悼", 30));
+		set.add(new Member("全辨悼", 30));
 
+		//历厘等 按眉 荐 免仿
+		System.out.println("醚 按眉 荐 : " + set.size());
 	}
 
 }
