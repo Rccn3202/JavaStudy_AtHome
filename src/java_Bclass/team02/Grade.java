be333771cb49625164aabe29b2d815829b7b18fb
comput3202 <comput3202@gmail.com>
2023-02-15 13:01:20 +0900

    2023.02.15.수

--- src/java_Bclass/team02/Grade.java
+++ src/java_Bclass/team02/Grade.java
@@ -17,8 +17,6 @@ public class Grade {
         stu.setKor(sc.nextInt());
         System.out.print("영어 : ");
         stu.setEng(sc.nextInt());
-        System.out.print("수학 : ");
-        stu.setMath(sc.nextInt());
         list.add(stu);
     }
  
@@ -49,9 +47,9 @@ public class Grade {
         while (it.hasNext()) {
             Student stu = it.next();
             if (stu.getName().equals(n)) { 
-                System.out.print("수정 - 1.국어  2.영어  3.수학 : ");
-                int f = sc.nextInt();
-                switch (f) {
+                System.out.print("수정 - 1.국어  2.영어  : ");
+                int subject = sc.nextInt();
+                switch (subject) {
                 case 1: // 국어 
                     System.out.print("국어 : ");
                     stu.setKor(sc.nextInt());
@@ -60,54 +58,58 @@ public class Grade {
                     System.out.print("영어 : ");
                     stu.setEng(sc.nextInt());
                     break;
-                case 3: // 수학 
-                    System.out.print("수학 : "); 
-                    stu.setMath(sc.nextInt());
-                    break;
+               
                 }
             }
         }
     }
  
     public void delete() {
-        Scanner sc = new Scanner(System.in);
-        boolean accessOK=true;
-        System.out.println("<<정보 삭제>>");
-        if (stu == null) {
-            System.out.println("삭제할 내역이 없습니다");
-        } else {
-            System.out.print("삭제 할 학생명> ");
-            String n = sc.next();
-            it = list.iterator();
-            while(it.hasNext()) {
-				Student stu=it.next();
-				if(stu.getName().equals(n)) {
-					System.out.println("정말 삭제?");
-					while(accessOK) {
-						String yn=sc.next();
-						if(yn.equals("y")) {
-							for(int i=1;i<=3;i++) {
-								System.out.println("관리자 번호를 입력하세요");
-								String password=sc.next();
-									if(password.equals("1234")) {
-										list.remove(stu);
-										System.out.println("삭제되었습니다"); break;
-									}else {
-										if(i==3) {
-											System.out.println("접속거부"); accessOK=false;
-										}else System.out.println("비번 틀렸음"+(3-i)+"회 남음"); 
-									}
-								} //sc.close(); 
-						
-						}else if(yn.equals("n")) {
-							System.out.println("프로그램 종료");
-							System.exit(0); //???
-						}else {
-							System.out.println("다시 입력");		}
-							}	
-						}
-            		}
-        }
+    	Scanner sc = new Scanner(System.in);
+    	boolean accessOK=true;
+    	System.out.println("<<정보 삭제>>");
+    	if (stu == null) { 
+    		System.out.println("삭제할 내역이 없습니다");
+    	} else {
+    		System.out.print("삭제 할 학생명> ");
+    		String n = sc.next();
+    		it = list.iterator();
+    		while(it.hasNext()) {
+    			Student stu=it.next();
+    			
+    			if(stu.getName().equals(n)) {
+    				System.out.println("정말 삭제?");
+    				while(accessOK) {
+    					String yn=sc.next();
+
+    					//~~가 삭제되었습니다 쓸까??
+    					if(yn.equalsIgnoreCase("y")) {
+    						for(int i=1;i<=3;i++) {
+    						System.out.println("관리자 번호를 입력하세요");
+							String password=sc.next();
+							
+    							if(password.equals("1234")) {
+    								list.remove(stu);
+    								System.out.println("삭제되었습니다"); 
+    								accessOK=false; break;
+	
+    							}else {//만약에 1234가 아니면
+    								
+    								if(i==3) {
+    									System.out.println("접속거부"); accessOK=false;
+    								}else System.out.println("비번 틀렸음"+(3-i)+"회 남음 다시입력"); 
+    							} 
+    						}
+
+    					}else if(yn.equalsIgnoreCase("n")) {
+    						System.out.println("프로그램 종료");
+    						System.exit(0); //???
+    					}else {
+    						System.out.println("다시 입력");		}
+    				}	
+    			}else System.out.println("해당 학생이 없음");
+    		}
+    	}
     }//delete
 }//Grade
     
