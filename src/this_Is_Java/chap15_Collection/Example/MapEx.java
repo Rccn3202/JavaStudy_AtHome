a35fe8bacd05dcf164d1ead87f0bebdefecf34c3
comput3202 <comput3202@gmail.com>
2023-02-17 01:44:32 +0900

    2023.02.17.금 01:44

---
+++ src/this_Is_Java/chap15_Collection/Example/MapEx.java
@@ -0,0 +1,36 @@
+package chap15_Collection.Example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapEx {
+
+	public static void main(String[] args) {
+		Map<String,Integer> map = new HashMap<String,Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null;	//최고 점수를 받은 아이디를 저장하는 변수
+		int maxScore = 0;	//최고 점수를 저장하는 변수
+		int totalScore = 0;	//점수 합계를 저장하는 변수
+		
+		//코드 작성
+		Set<Map.Entry<String,Integer>> entrySet = map.entrySet();
+		for(Map.Entry<String, Integer> entry : entrySet) {
+			if(entry.getValue() > maxScore) {
+			name = entry.getKey();
+			maxScore = entry.getValue();
+			} 
+			totalScore += entry.getValue();
+		}
+
+		int avgScore = totalScore / map.size();
+		System.out.println("평균 점수: "+avgScore);
+		System.out.println("최고 점수: "+maxScore);
+		System.out.println("최고 점수를 받은 아이디: "+name);
+		
+	}
+
+}
