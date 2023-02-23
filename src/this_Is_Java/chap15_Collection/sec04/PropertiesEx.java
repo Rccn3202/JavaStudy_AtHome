e7ec108a969f809b4541971616d0a95fd7d4becd
comput3202 <comput3202@gmail.com>
2023-02-23 13:12:21 +0900

    2023.02.22.wen

--- src/this_Is_Java/chap15_Collection/sec04/PropertiesEx.java
+++ src/this_Is_Java/chap15_Collection/sec04/PropertiesEx.java
@@ -9,7 +9,24 @@ public class PropertiesEx {
 		Properties properties = new Properties();
 		
 		//PropertiesEx.class와 동일한 ClassPath에 있는 database.properties 파일 로드
+		properties.load(PropertiesEx.class.getResourceAsStream("database.properties));
 
+		//주어진 키에 대한 값 읽기
+		String driver = properties.getProperty("driver");
+		String url = properties.getProperty("url");
+		String usename = properties.getProperty("username");
+		String password = properties.getProperty("password");
+		String admin = properties.getProperty("admin");
+		
+		//값 출력
+		System.out.println("driver : "+driver);
+		System.out.println("url : "+url);
+		System.out.println("username : "+username);
+		System.out.println("password : "+password);
+		System.out.println("admin : "+admin);
+		
+				
+				
 	}
 
 }
