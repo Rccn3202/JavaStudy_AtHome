2bb0773080bce17e8b115006469d17e0d0fc8f39
comput3202 <comput3202@gmail.com>
2023-03-07 02:50:17 +0900

    2023.03.07.tue 02:50

---
+++ src/this_Is_Java/chap19_Network/Info.java
@@ -0,0 +1,10 @@
+package chap19_Network;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/this_Is_Java/chap19_Network/sec02/Info.java
@@ -0,0 +1,10 @@
+package chap19_Network.sec02;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
