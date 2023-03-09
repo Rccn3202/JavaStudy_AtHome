39ee3b405431a37781dd93d9cae697054cc8dd9a
comput3202 <comput3202@gmail.com>
2023-03-09 13:38:08 +0900

    2023.03.09.thu

--- src/this_Is_Java/chap17_Stream/sec12/CollectEx01.java
+++ src/this_Is_Java/chap17_Stream/sec12/CollectEx01.java
@@ -1,9 +1,16 @@
 package chap17_Stream.sec12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectEx01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Student> totalList = new ArrayList<>();
+		totalList.add(new Student("홍길동","남",92));
+		totalList.add(new Student("김수영","여",87));
+		totalList.add(new Student("감자바","남",95));
+		totalList.add(new Student("오해영","여",93));
 
 	}
 
